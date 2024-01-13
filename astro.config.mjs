fc82f462a70d3af4b8646d6ae465f2ee945e63cd
aminahbl <aminahbl.github@37e.uk>
2024-01-14 00:22:30 +0100

    comment out redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,11 @@ export default defineConfig({
   output: "hybrid",
   adapter: netlify(),
   redirects: {
-    // Astro's SSR `_redirects` file conflicts with redirects Netlify redirects (see: https://github.com/withastro/astro/issues/7970#issuecomment-1666570628). In theory, this might solve it (https://dev.to/cassidoo/setting-up-netlify-redirects-with-astro-426g#comment-2a2en).
-    '/api/net/*': {
-      status: 200,
-      // https://docs.netlify.com/routing/redirects/redirect-options/
-      destination: '/.netlify/functions/:splat ',
-    },
+    // TODO: Astro's SSR `_redirects` file conflicts with redirects Netlify redirects (see: https://github.com/withastro/astro/issues/7970#issuecomment-1666570628). In theory, this might solve it (https://dev.to/cassidoo/setting-up-netlify-redirects-with-astro-426g#comment-2a2en), but Astro doesn't like Netlify's the sytax.
+    // '/api/net/*': {
+    //   status: 200,
+    //   // https://docs.netlify.com/routing/redirects/redirect-options/
+    //   destination: '/.netlify/functions/:splat ',
+    // },
   }
 })
