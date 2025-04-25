59a25fa3f0cf8a489734473f8f7cbaa57a8a059e
aminahbl <aminahbl.github@37e.uk>
2025-04-25 12:52:17 +0100

    upgrade packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import netlify from "@astrojs/netlify"
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  output: "static",
   adapter: netlify(),
   redirects: {
     // TODO: Astro's SSR `_redirects` file conflicts with redirects Netlify redirects (see: https://github.com/withastro/astro/issues/7970#issuecomment-1666570628). In theory, this might solve it (https://dev.to/cassidoo/setting-up-netlify-redirects-with-astro-426g#comment-2a2en), but Astro doesn't like Netlify's the sytax.
